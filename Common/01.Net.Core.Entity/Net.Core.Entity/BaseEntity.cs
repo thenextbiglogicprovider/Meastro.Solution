a51f76916f77c78b17573b4ceef4986c865ba632
AjeetYadav <ajeet.yadav@nagarro.com>
2017-09-23 02:41:10 +0530

    Add project files.

---
+++ Common/01.Net.Core.Entity/Net.Core.Entity/BaseEntity.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Net.Core.Entity
+{
+    public class BaseEntity : IBaseEntity
+    {
+    }
+}
---
+++ Common/01.Net.Entity/Net.Entity/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿/// <summary>
+/// The Base Entity
+/// </summary>
+namespace Net.Entity
+{
+    /// <summary>
+    /// Represents the Base Entity class
+    /// </summary>
+    /// <seealso cref="Net.Entity.IBaseEntity" />
+    public class BaseEntity : IBaseEntity
+    {
+    }
+}
