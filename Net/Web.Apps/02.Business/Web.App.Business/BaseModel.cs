558d54a13ad124defd7038bc4a993c098ff831a2
AjeetYadav <ajeet.yadav@nagarro.com>
2017-09-26 02:07:27 +0530

    base model + extension class added, UI Layer added

---
+++ Net/Web.Apps/02.Business/Web.App.Business/BaseModel.cs
@@ -0,0 +1,179 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+/// <summary>
+/// Represents the Base Model Class
+/// </summary>
+namespace Web.App.Business
+{
+    /// <summary>
+    /// The Base Model
+    /// </summary>
+    public class BaseModel
+    {
+        #region Constructor
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BaseModel"/> class.
+        /// </summary>
+        public BaseModel()
+        {
+
+        }
+        #endregion
+
+        #region Overridable Members
+        /// <summary>
+        /// Gets or sets the type.
+        /// </summary>
+        /// <value>
+        /// The type.
+        /// </value>
+        public virtual string Type { get { return this.Type = string.Format("{0}-{1}", this.GetType().Assembly.GetName().ToString(), this.GetType().Name); } set { this.Type = value; } }
+        #endregion
+
+        #region Public Members
+        /// <summary>
+        /// Gets or sets the unique identifier.
+        /// </summary>
+        /// <value>
+        /// The unique identifier.
+        /// </value>
+        public Guid GUID { get; set; }
+
+        /// <summary>
+        /// Gets or sets the created at UTC.
+        /// </summary>
+        /// <value>
+        /// The created at UTC.
+        /// </value>
+        public DateTime CreatedAtUTC { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance is active.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if this instance is active; otherwise, <c>false</c>.
+        /// </value>
+        public bool IsActive { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance is deleted.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if this instance is deleted; otherwise, <c>false</c>.
+        /// </value>
+        public bool IsDeleted { get; set; }
+
+        /// <summary>
+        /// Gets or sets the updated at UTC.
+        /// </summary>
+        /// <value>
+        /// The updated at UTC.
+        /// </value>
+        public DateTime UpdatedAtUTC { get; set; }
+        #endregion
+
+        #region Private Members
+        /// <summary>
+        /// Gets or sets the hash set.
+        /// </summary>
+        /// <value>
+        /// The hash set.
+        /// </value>
+        private HashSet<BaseModel> HashSet { get; set; }
+
+        /// <summary>
+        /// Gets or sets the serialized data.
+        /// </summary>
+        /// <value>
+        /// The serialized data.
+        /// </value>
+        private string SerializedData { get; set; }
+
+        /// <summary>
+        /// Gets or sets the deserialized data.
+        /// </summary>
+        /// <value>
+        /// The deserialized data.
+        /// </value>
+        private BaseModel DeserializedData { get; set; }
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Gets the hash.
+        /// </summary>
+        /// <returns>
+        /// The Hash Set of type
+        /// </returns>
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        /// <summary>
+        /// Gets the serialized data.
+        /// </summary>
+        /// <returns>
+        /// The Serialized String data
+        /// </returns>
+        public virtual string GetSerializedData()
+        {
+            return this.SerializedData = JsonConvert.SerializeObject(this);
+        }
+
+        /// <summary>
+        /// Gets the deserialzed data.
+        /// </summary>
+        /// <returns></returns>
+        public virtual BaseModel GetDeserialzedData()
+        {
+            return this.DeserializedData = this;
+        }
+
+        /// <summary>
+        /// Adds to hash set.
+        /// </summary>
+        /// <param name="obj">The object.</param>
+        /// <returns>
+        /// The Hash Set with newly added item
+        /// </returns>
+        /// <exception cref="System.NullReferenceException"></exception>
+        /// <exception cref="System.ArgumentNullException"></exception>
+        public virtual HashSet<BaseModel> AddToHashSet(BaseModel obj)
+        {
+            if (this.HashSet == null)
+                this.HashSet = new HashSet<BaseModel>();
+            if (obj == null)
+                throw new ArgumentNullException();
+            this.HashSet.Add(obj);
+
+            return this.HashSet;
+        }
+
+        /// <summary>
+        /// To the dictionary.
+        /// </summary>
+        /// <returns>
+        /// The Dictionary from the Hash Set of T
+        /// </returns>
+        /// <exception cref="System.NullReferenceException">HashSet is null</exception>
+        public virtual IDictionary<Guid, BaseModel> ToDictionary()
+        {
+            var dictionary = new Dictionary<Guid, BaseModel>();
+            if (this.HashSet == null)
+                throw new NullReferenceException("HashSet is null");
+            foreach (var key in this.HashSet)
+            {
+                dictionary.Add(key.GUID, this.HashSet.FirstOrDefault(x => x.GUID == key.GUID));
+            }
+
+            return dictionary;
+        }
+        #endregion
+    }
+}
---
+++ Net/Web.Apps/02.Business/Web.App.Business/Extensions/BaseModelExtension.cs
@@ -0,0 +1,199 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Represents the Base Model Extension Class
+/// </summary>
+namespace Web.App.Business.Extensions
+{
+    /// <summary>
+    /// The Base Model Extension Class
+    /// </summary>
+    public static class BaseModelExtension
+    {
+        /// <summary>
+        /// Returns a <see cref="System.String" /> that represents this instance.
+        /// </summary>
+        /// <param name="model">The model.</param>
+        /// <param name="format">The format.</param>
+        /// <returns>
+        /// A <see cref="System.String" /> that represents this instance.
+        /// </returns>
+        public static string ToString(this string str, IFormatProvider format)
+        {
+            return str.ToString(format);
+        }
+
+        /// <summary>
+        /// To the locale date time.
+        /// </summary>
+        /// <param name="dateTime">The date time.</param>
+        /// <param name="culture">The culture.</param>
+        /// <returns></returns>
+        public static string ToLocaleDateTime(this DateTime dateTime, CultureInfo culture)
+        {
+            return dateTime.ToLocaleDateTime(culture);
+        }
+
+        /// <summary>
+        /// To the serialized data.
+        /// </summary>
+        /// <param name="obj">The object.</param>
+        /// <returns></returns>
+        /// <exception cref="JsonSerializationException"></exception>
+        public static string ToSerializedData(this object obj)
+        {
+            try
+            {
+                return JsonConvert.SerializeObject(obj);
+            }
+            catch
+
+            {
+                throw new JsonSerializationException();
+            }
+        }
+
+        /// <summary>
+        /// To the object.
+        /// </summary>
+        /// <param name="serializedData">The serialized data.</param>
+        /// <returns></returns>
+        /// <exception cref="JsonSerializationException"></exception>
+        public static Object ToObject(this string serializedData)
+        {
+            try
+            {
+                return JsonConvert.DeserializeObject(serializedData);
+            }
+            catch
+            {
+                throw new JsonSerializationException();
+            }
+        }
+
+        /// <summary>
+        /// To the number.
+        /// </summary>
+        /// <param name="number">The number.</param>
+        /// <returns>
+        /// The Number representation
+        /// </returns>
+        /// <exception cref="System.InvalidCastException"></exception>
+        public static int ToNumber(this string number)
+        {
+            try
+            {
+                return Convert.ToInt32(number);
+            }
+            catch
+            {
+                throw new InvalidCastException();
+            }
+        }
+
+        /// <summary>
+        /// To the decimal.
+        /// </summary>
+        /// <param name="number">The number.</param>
+        /// <returns>
+        /// The Decimal representation
+        /// </returns>
+        /// <exception cref="System.InvalidCastException"></exception>
+        public static decimal ToDecimal(this string number)
+        {
+            try
+            {
+                return Convert.ToDecimal(number);
+            }
+            catch
+            {
+                throw new InvalidCastException();
+            }
+        }
+
+        /// <summary>
+        /// To the decimal.
+        /// </summary>
+        /// <param name="number">The number.</param>
+        /// <returns>
+        /// The Decimal representation
+        /// </returns>
+        /// <exception cref="System.InvalidCastException"></exception>
+        public static decimal ToDecimal(this int number)
+        {
+            try
+            {
+                return Convert.ToDecimal(number);
+            }
+            catch
+            {
+                throw new InvalidCastException();
+            }
+        }
+
+        /// <summary>
+        /// To the double.
+        /// </summary>
+        /// <param name="number">The number.</param>
+        /// <returns>
+        /// The Double floating point representation
+        /// </returns>
+        /// <exception cref="System.InvalidCastException"></exception>
+        public static double ToDouble(this string number)
+        {
+            try
+            {
+                return Convert.ToDouble(number);
+            }
+            catch
+            {
+                throw new InvalidCastException();
+            }
+        }
+
+        /// <summary>
+        /// To the float.
+        /// </summary>
+        /// <param name="number">The number.</param>
+        /// <param name="fixedPlaces">The fixed places.</param>
+        /// <returns></returns>
+        /// <exception cref="System.InvalidCastException"></exception>
+        public static float ToFloat(this string number, int fixedPlaces = 0)
+        {
+            try
+            {
+                return fixedPlaces > 0 ? float.Parse(Decimal.Round(ToDecimal(number), fixedPlaces).ToString()) : float.Parse(number);
+            }
+            catch
+            {
+                throw new InvalidCastException();
+            }
+        }
+
+        /// <summary>
+        /// To the date time.
+        /// </summary>
+        /// <param name="dateTime">The date time.</param>
+        /// <returns>
+        /// The DateTime representation
+        /// </returns>
+        /// <exception cref="System.InvalidCastException"></exception>
+        public static DateTime ToDateTime(this string dateTime)
+        {
+            try
+            {
+                return Convert.ToDateTime(dateTime);
+            }
+            catch
+            {
+                throw new InvalidCastException();
+            }
+        }
+    }
+}
